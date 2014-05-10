3c0e544613fb8de941d7a0e758a6d62a3894ca04
Mateusz Soltysik <mateusz@soltysik.org>
2014-05-10 18:27:34 +0200

    Semi-finish on BBS, ParkMiller and Xorshift.

--- PRNG/src/algorithms/Xorshift.java
+++ PRNG/src/algorithms/Xorshift.java
@@ -2,8 +2,24 @@ package algorithms;
 
 @SuppressWarnings("UnusedDeclaration")
 public class Xorshift {
-    public Xorshift() {
-        System.out.println("Hello");
+    private static final long max = ((long) 2 << 30) - 1;
+    private static long seed = System.currentTimeMillis();
+
+    public static long getRandomNumber() {
+        seed ^= seed >> 12;
+        seed ^= seed << 25;
+        seed ^= seed >> 27;
+        seed = (seed * 2685821657736338717L) % max;
+        return Math.abs(seed);
     }
 
+    public static long getRandomNumber(long maxValue) {
+        long randomNumber = getRandomNumber();
+        return randomNumber % (maxValue + 1);
+    }
+
+    public static long getRandomNumber(long minValue, long maxValue) {
+        long randomNumber = getRandomNumber();
+        return minValue + (randomNumber % (maxValue - minValue + 1));
+    }
 }
