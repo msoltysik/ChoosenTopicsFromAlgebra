be4482b796a72ed85ca3c366794b9aa3a4b810c7
Mateusz Soltysik <mateusz@soltysik.org>
2014-05-10 21:10:58 +0200

    Renaming filenames and some fixes in algorithms.

--- PRNG/src/algorithms/ParkMiller.java
+++ PRNG/src/algorithms/ParkMiller.java
@@ -1,22 +1,34 @@
 package algorithms;
 
-// Park–Miller random number generator
 @SuppressWarnings("ALL")
+
 public class ParkMiller {
     private static final long max = ((long) 2 << 30) - 1;
     private static final long a = 16807;
-    private static long seed = System.currentTimeMillis();
+    private static long seed = System.currentTimeMillis() + System.identityHashCode(new Object());
 
+    /**
+     * @return pseudo-random number between minValue and maxValue.
+     */
     public static long getRandomNumber() {
         seed = (a * seed) % max;
         return seed;
     }
 
+    /**
+     * @param maxValue maximum value which the function can return.
+     * @return pseudo-random number between minValue and maxValue.
+     */
     public static long getRandomNumber(long maxValue) {
         seed = getRandomNumber();
         return seed % (maxValue + 1);
     }
 
+    /**
+     * @param minValue minimum value which the function can return.
+     * @param maxValue maximum value which the function can return.
+     * @return pseudo-random number between minValue and maxValue.
+     */
     public static long getRandomNumber(long minValue, long maxValue) {
         seed = getRandomNumber();
         return minValue + (seed % (maxValue - minValue + 1));
