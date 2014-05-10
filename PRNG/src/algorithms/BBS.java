4bf0684f8c0a6c22ee4d5d797e1a1595d2f5b81b
Mateusz Soltysik <mateusz@soltysik.org>
2014-05-10 18:27:34 +0200

    Added BBS, ParkMiller, Xorshift implementation

--- PRNG/src/algorithms/BBS.java
+++ PRNG/src/algorithms/BBS.java
@@ -1,12 +1,24 @@
 package algorithms;
 
 //Blum Blum Shub
+@SuppressWarnings("UnusedDeclaration")
 public class BBS {
-    static int p = 11;
-    static int q = 19;
+    private static final int p = 11;
+    private static final int q = 19;
+    private static long seed = System.currentTimeMillis();
 
-    public static void getRandomNumber(int x) {
-            int randomNumber = (x * x) % (p * q);
-            System.out.println(randomNumber);
+    public static long getRandomNumber() {
+        seed = (seed * seed % (p * q));
+        return Math.abs(seed);
+    }
+
+    public static long getRandomNumber(long maxValue) {
+        long randomNumber = getRandomNumber();
+        return randomNumber % (maxValue + 1);
+    }
+
+    public static long getRandomNumber(long minValue, long maxValue) {
+        long randomNumber = getRandomNumber();
+        return minValue + (randomNumber % (maxValue - minValue + 1));
     }
 }
--- PRNG/src/algorithms/ParkMiller.java
+++ PRNG/src/algorithms/ParkMiller.java
@@ -3,4 +3,24 @@ package algorithms;
 // Park–Miller random number generator
 @SuppressWarnings("ALL")
 public class ParkMiller {
+    private static final long max = ((long) 2 << 30) - 1;
+    private static final long a = 16807;
+    private static long seed = System.currentTimeMillis();
+
+    public static long getRandomNumber() {
+        seed = (a * seed) % max;
+        return seed;
+    }
+
+    public static long getRandomNumber(long maxValue) {
+        seed = getRandomNumber();
+        return seed % (maxValue + 1);
+    }
+
+    public static long getRandomNumber(long minValue, long maxValue) {
+        seed = getRandomNumber();
+        return minValue + (seed % (maxValue - minValue + 1));
+    }
+
+
 }
