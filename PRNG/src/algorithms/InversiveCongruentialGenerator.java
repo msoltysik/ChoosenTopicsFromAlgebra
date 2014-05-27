25184051543aa061578ec768b9c16d968768b11f
Mateusz Soltysik <mateusz@soltysik.org>
2014-05-28 01:58:08 +0200

    Remove Inverse Congruential Generator out of project scope

--- PRNG/src/algorithms/InversiveCongruentialGenerator.java
+++
@@ -1,7 +0,0 @@
-package algorithms;
-
-@SuppressWarnings("UnusedDeclaration")
-
-public abstract class InversiveCongruentialGenerator {
-    private static long seed = System.currentTimeMillis() + System.identityHashCode(new Object());
-}
