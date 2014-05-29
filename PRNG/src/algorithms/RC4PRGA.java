671cb57d058d5358471f9e6b4a3f3d63af5b52ed
Andrzej Kwak <andy.kwaq@gmail.com>
2014-05-29 23:54:19 +0200

    fixed arcfour

--- PRNG/src/algorithms/RC4PRGA.java
+++ PRNG/src/algorithms/RC4PRGA.java
@@ -1,52 +1,65 @@
 package algorithms;
 
+import java.util.List;
+
 import algorithms.interfaces.IGenerator;
 
 @SuppressWarnings("UnusedDeclaration")
 
 public class RC4PRGA implements IGenerator {
-    private int[] s = new int[256];
+    List<Integer> list = new ArrayList<Integer>();
     private long seed;
-
+    private char[] key = "keyforplaintext12345".toCharArray();
+    
+    
     public RC4PRGA() {
         for (int i = 0; i < 256; i++) {
-            s[i] = i;
-            seed = System.currentTimeMillis() + System.identityHashCode(new Object());
+            list.add(i);
+        }
+        int j = 0;
+        java.util.Collections.shuffle(list);
+        for(int i = 0; i < 256; i++){
+        	j = (j + list.get(i) + Character.getNumericValue(key[i%key.length])) % 256;
+        	int tmp = list.get(i);
+        	list.set(i, list.get(j));
+        	list.set(j, tmp);
         }
     }
-
-    public long permute() {
-        long counter = 0L;
-        int i = 0;
-        int j = 0;
-        long out = 0L;
-        while (counter < seed) {
-            int tmp;
-            i = (i + 1) % 256;
-            j = (j + s[i]) % 256;
-
-            tmp = s[j];
-            s[j] = s[i];
-            s[i] = tmp;
-
-            out += s[(s[i] + s[j]) % 256];
-            counter++;
-        }
-        return out;
+    private String prga(){
+    	int i = (i+1) % 256;
+    	int j = j + list.get(i) % 256;
+    	
+    	int tmp = list.get(i);
+    	list.set(i, list.get(j));
+    	list.set(j, tmp);
+    	int t = list.get((i+j)%256);
+    	return Integer.toHexString(list.get(t));
     }
 
     public long getRandomNumber() {
-        long random = permute();
-        return random & 0xffffffffL;
+    	String nextpart = '';
+    	String out = '';
+    	for(int i = 0; i < 8; i++){
+    		nextpart = prga();
+    		out.concat(nextpart);
+    	}
+    	return Long.parseLong(out,16);
     }
-
+    
     public long getRandomNumber(long maxValue) {
-        long randomNumber = getRandomNumber();
-        return randomNumber % (maxValue + 1);
+    	long randomNumber = getRandomNumber();
+    	return randomNumber % maxValue;
     }
-
+    
     public long getRandomNumber(long minValue, long maxValue) {
+
+        if (minValue > maxValue) {
+            long tmp = minValue;
+            minValue = maxValue;
+            maxValue = tmp;
+        }
+
         long randomNumber = getRandomNumber();
         return minValue + (randomNumber % (maxValue - minValue + 1));
     }
-}
+    
