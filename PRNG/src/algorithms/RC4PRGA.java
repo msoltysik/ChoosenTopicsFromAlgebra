eff67b0f221f76293771eacc7bb4ac153c8b3314
Andrzej Kwak <andy.kwaq@gmail.com>
2014-06-05 10:36:04 +0200

    fix rc4

--- PRNG/src/algorithms/RC4PRGA.java
+++ PRNG/src/algorithms/RC4PRGA.java
@@ -3,28 +3,35 @@ package algorithms;
 import java.util.ArrayList;
 import java.util.List;
 
-import algorithms.interfaces.IGenerator;
+//import algorithms.interfaces.IGenerator;
 
 @SuppressWarnings("UnusedDeclaration")
 
 public class RC4PRGA implements IGenerator {
-    List<Integer> list = new ArrayList<Integer>();
-    private long seed;
-    private char[] key = "keyforplaintext12345".toCharArray();
+    private List<Integer> list = new ArrayList<Integer>();
+    private long seed = System.currentTimeMillis() + System.identityHashCode(new Object()); 
+   // private char[] key = "keyforplaintext12345".toCharArray();
     
+    
+    private void init() {
+    	seed = System.currentTimeMillis() + System.identityHashCode(new Object());
+    	String key = Long.toHexString(seed);	
+    	int j = 0;
+        for(int i = 0; i < 256; i++){
+        	j = (j + list.get(i) + (int) key.charAt(i%key.length())) % 256;
+        	int tmp = list.get(i);
+        	list.set(i, list.get(j));
+        	list.set(j, tmp);
+        }
+    }
     
     public RC4PRGA() {
         for (int i = 0; i < 256; i++) {
             list.add(i);
         }
-        int j = 0;
+        
         java.util.Collections.shuffle(list);
-        for(int i = 0; i < 256; i++){
-        	j = (j + list.get(i) + Character.getNumericValue(key[i%key.length])) % 256;
-        	int tmp = list.get(i);
-        	list.set(i, list.get(j));
-        	list.set(j, tmp);
-        }
+        init();
     }
     private String prga(){
     	int i,j;
@@ -41,13 +48,12 @@ public class RC4PRGA implements IGenerator {
     }
 
     public long getRandomNumber() {
+    	init();
     	String nextpart = "";
     	String out = "";
     	for(int i = 0; i < 7; i++){
     		nextpart = prga();
-    		System.out.println(nextpart);
     		out = out + nextpart;
-    		System.out.println(out);
     	}
     	return Long.parseLong(out,16);
     }
